aceae1ac402ff0b2c08b814b80d7520ecd070778
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-08-20 23:23:27 +0300

    Task34

---
+++ Task34.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите количество элементов массива:");
+int i = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [i];
+int SumEv = 0;
+int SumOdd = 0;
+for (int count = 0; count < i; count++) {
+    array[count] = new Random().Next(100, 1000);
+}
+for (int count = 0; count < i; count ++) {
+    if (array[count] % 2 == 0) {
+        SumEv += 1;
+    }
+    else {
+        SumOdd += 1;
+    }
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+Console.WriteLine($"Количество четных элементов массива = {SumEv}");
+Console.WriteLine($"Количество нечетных элементов массива = {SumOdd}");
+
